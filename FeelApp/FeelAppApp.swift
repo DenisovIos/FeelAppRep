6ee427fe62ed5b9cfd4837c5f75d67de458eac27
Denisov Ruslan <denisovitapi@gmail.com>
2023-06-04 14:54:57 +0200

    Initial Commit

---
+++ FeelApp/FeelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeelAppApp.swift
+//  FeelApp
+//
+//  Created by Руслан on 04.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct FeelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
